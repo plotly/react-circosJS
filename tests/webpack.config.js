f56b212b6e775dacfd3eba2996b032eab280bb31
nicgirault <nic.girault@gmail.com>
2018-07-13 22:38:12 +0200

    📷 setup screenshot tests

---
+++ tests/webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+
+module.exports = {
+  entry: path.resolve(__dirname, 'index.js'),
+  output: {
+    path: path.resolve(__dirname),
+    filename: 'bundle.js'
+  },
+  mode: 'production',
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      }
+    ]
+  }
+};
